35b1cee1d0d594509f0fb6f56cf69506d2bf4c96
June <59625251+sukjunel@users.noreply.github.com>
2020-03-22 13:30:57 +0000

    creating sql database and manipulating admin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,27 +2,27 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-#dummy data
-posts = [
-    {
-        'author': 'Sukjunel',
-        'title': 'how to become a good programmer',
-        'content': 'you just do it!',
-        'date_posted': 'Jun 25, 2000'
-    },
-    {
-        'author': 'June',
-        'title': 'how to become a bad programmer',
-        'content': 'wear adidas!',
-        'date_posted': 'Jun 25, 2020'
-    }
-]
-
+# #dummy data
+# posts = [
+#     {
+#         'author': 'Sukjunel',
+#         'title': 'how to become a good programmer',
+#         'content': 'you just do it!',
+#         'date_posted': 'Jun 25, 2000'
+#     },
+#     {
+#         'author': 'June',
+#         'title': 'how to become a bad programmer',
+#         'content': 'wear adidas!',
+#         'date_posted': 'Jun 25, 2020'
+#     }
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # return HttpResponse('<h1>blog home</h1>')
     return render(request, 'blog/home.html', context)
